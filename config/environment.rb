93a7b48d0246d2a94a5b131729bd8a67c8a38aa2
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-07-28 14:30:45 -0500

    created run methods

--- config/environment.rb
+++ config/environment.rb
@@ -6,12 +6,4 @@ require_all 'app/models'
 ENV['SINATRA_ENV'] ||= 'development'
 
 ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
-
-
-# lionel_messi = Player.create(attributes)
-# barcelona = Team.create({team_name: "Barcelona", league: "la liga"})
-# barca_contract = Contract.create({player: lionel_messi, team: barcelona, amount: 10000000, date: "7/28/2020"})
-
-
-# lionel_messi.net_worth
-# binding.pry
+ActiveRecord::Base.logger = nil
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,3 @@ lionel_messi = Player.create({first_name: "lionel", last_name: "messi", position
 barcelona = Team.create({team_name: "Barcelona", league: "la liga"})
 barca_contract = Contract.create({player: lionel_messi, team: barcelona, amount: 10000000, date: "7/28/2020"})
 
-cristiano_ronaldo = 
-juventus = 
-cronaldo_contract = 
-
