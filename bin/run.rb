56f1fd0b0515e4039c922d7a5bf2b7a703161f92
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-07-28 11:22:10 -0500

    added data to bin

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,7 @@
 require_relative '../config/environment'
 
-
+puts "Hello user and welcome to our Fifa player database!"
+puts "Below is a list of commands you can enter at any time:"
+puts "Type 'help' at any time to bring up a list of commands"
 
 puts "HELLO WORLD"
