c7c379c1cd5c033954db96f4d3b8a55e76b278cb
Abbosbek-SE <66321147+Abbosbek-SE@users.noreply.github.com>
2020-07-30 00:19:26 -0700

    Update team.rb

--- app/models/team.rb
+++ app/models/team.rb
@@ -6,4 +6,9 @@ class Team < ActiveRecord::Base
         puts self.team_name
         puts "Players: #{self.players.map {|i| i.name}.join(', ')}"
     end
+  
+    def self.create(team_name, league)
+       Team.new(team_name, league)
+    end
+  
 end
